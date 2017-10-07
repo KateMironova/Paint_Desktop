72a71c07cee46f5be3a547e24d0bb2040a58b750
Mironova Ekaterina <ekmironova123@gmail.com>
2017-10-07 15:01:20 +0300

    Added Test

---
+++ TestPaint/UnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+using MakePaintGreatAgain.Controls;
+using System.Windows.Forms;
+
+namespace TestPaint
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        TestCommand xcom;
+        PMenu menuBar;
+
+        [SetUp]
+        public void SetUp()
+        {
+            xcom = new TestCommand();
+            menuBar = new PMenu();
+        }
+
+        [TestCase("Save", "save")]
+        public void TestMenuFile(string name, string result)
+        {
+            ToolStripMenuItem file = menuBar.menuStrip1.Items["File"] as ToolStripMenuItem;
+            file.DropDownItems[name].PerformClick();
+            Assert.AreEqual(result, xcom.result);
+        }
+        
+    }
+}
